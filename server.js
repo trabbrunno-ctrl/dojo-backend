2929f641bc51fdf9e4a7119aa7da8ff84ead3d0a
trabbrunno-ctrl <trab.brunno@gmail.com>
2026-01-16 22:40:19 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 require("dotenv").config();
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Servidor rodando na porta", PORT));
+
 const express = require("express");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
