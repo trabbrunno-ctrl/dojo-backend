b4480bbb65b7676914b35029aacd04e256331dcc
trabbrunno-ctrl <trab.brunno@gmail.com>
2026-01-16 22:27:54 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { Pool } = require("pg");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+app.get("/", (_, res) => {
+  res.send("Backend online 🚀");
+});
+
+app.listen(3000, () => console.log("Servidor rodando"));
